bc739954bcd38f725070d3163d363c7b4e741478
Yonseca <yonseca@gmail.com>
2015-12-22 01:58:45 +0100

    Some new tests

--- test/numberCharsStringTests/numberCharsStringNext.java
+++ test/numberCharsStringTests/numberCharsStringNext.java
@@ -24,7 +24,7 @@ public class numberCharsStringNext {
 	}
 
 	@Test
-	public void testAaaaaaaaaaaaaaaa_PasaASer_aaaaaaaaaaaaaab() {
+	public void testAaaaaaaaaaaaaaaa_goes_aaaaaaaaaaaaaab() {
 		char[] testString = new char[16];
 		Arrays.fill(testString , 'a');
 		testString[testString.length-1] = 'b';
@@ -35,13 +35,25 @@ public class numberCharsStringNext {
 	
 
 	@Test
-	public void testAaaaaaaaaaaaaaaz_PasaASer_aaaaaaaaaaaaaa0() {
+	public void testAaaaaaaaaaaaaaaz_goes_aaaaaaaaaaaaaa0() {
 		char[] testString = new char[16];
 		Arrays.fill(testString , 'a');
 		testString[testString.length-1] = '0';
-		NumberCharsString cadenaPrueba = new NumberCharsString("aaaaaaaaaaaaaaz");
+		NumberCharsString cadenaPrueba = new NumberCharsString("aaaaaaaaaaaaaaaz");
 		cadenaPrueba.next();
 		assertArrayEquals(testString, cadenaPrueba.getCharsString());
 	}
+
+	@Test
+	public void testAaaaaaaaaaaaaaa9_goes_aaaaaaaaaaaaaba() {
+		char[] testString = new char[16];
+		Arrays.fill(testString , 'a');
+		testString[testString.length-2] = 'b';
+		NumberCharsString cadenaPrueba = new NumberCharsString("aaaaaaaaaaaaaaa9");
+		cadenaPrueba.next();
+		assertArrayEquals(testString, cadenaPrueba.getCharsString());
+	}
+	
+
 
 }
